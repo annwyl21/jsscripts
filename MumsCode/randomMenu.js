c3e68cef1b4663f1f412f3cfcef00371f84b6812
annwyl21 <ellen.a.ash@gmail.com>
2021-09-23 10:50:14 +0000

    Error Review of broken code

--- MumsCode/randomMenu.js
+++ MumsCode/randomMenu.js
@@ -1,7 +1,13 @@
+//DOES NOT WORK
+//First Attempt at Code Academy Random Meal Maker
+//used alot of console.log to test the final version
+//common errors I made were variable names like appetizer instead of appetizers and forgetting the , at the end of lines in the object
+//final version is mealMakerProject.js
+
 const menu = {
     _courses: {
-      appetizers: [],
-      get appetizers() {},
+      appetizers: [],//these need an _ at the start pf the name
+      get appetizers() {},//I haven't returned anything
       set appetizers(appetizersIn) {},
       mains: [],
       get mains() {},
@@ -10,32 +16,32 @@ const menu = {
       get desserts() {},
       set desserts(dessertsIn) {}
     },
-   get _courses() {
+   get _courses() {//This should be get courses without an _
      return {
        appetizers: this.appetizers,
        mains: this.mains,
        desserts: this.desserts
      };
    },
-   addDishToCourse: function(courseName, dishName, dishPrice){
+   addDishToCourse: function(courseName, dishName, dishPrice){ //wrong syntax, dont need : or the word function
      const dish = {
        name: dishName,
        price: dishPrice,
      };
      this._courses[courseName].push(dish);
    },
-   getRandomDishFromCourse: function(courseName) {
+   getRandomDishFromCourse: function(courseName) {//wrong syntax again
      let dishes = this._courses[courseName];
-     let randomNumber = Math.floor(Math.random() * dishes.length);
+     let randomNumber = Math.floor(Math.random() * dishes.length);//haven't divided .length by 2 because the number is elements in the array not objects in the array and each object has 2 keys
      return dishes[randomNumber];
    },
-   generateRandomMeal: function(){
+   generateRandomMeal: function(){//syntax for function again
      const app = this.getRandomDishFromCourse('appetizers');
      const mai = this.getRandomDishFromCourse('mains');
      const dess = this.getRandomDishFromCourse('desserts');
-     const totalPrice = appetizer.price + mains.price + desserts.price
+     const totalPrice = appetizer.price + mains.price + desserts.price//not using the correct name for each object it should be app.name instead of appetizer.name
   
-     return `Appetizer: ${appetizer.name} Main: ${mains.name} Dessert: ${desserts.price}. The cost of the meal will be ${totalPrice}.`
+     return `Appetizer: ${appetizer.name} Main: ${mains.name} Dessert: ${desserts.price}. The cost of the meal will be ${totalPrice}.`//might work but in the final code I used string concatenation because this return line was error prone
    }
   }
   
