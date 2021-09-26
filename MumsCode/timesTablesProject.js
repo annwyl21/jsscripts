60adad2ffbe8d3be3ffa9afa7c1c2cf843df20e8
annwyl21 <ellen.a.ash@gmail.com>
2021-09-26 16:12:19 +0000

    Times Tables Project

---
+++ MumsCode/timesTablesProject.js
@@ -0,0 +1,37 @@
+//TimesTables Project
+
+//display 1 of the tables in an array
+const questionArrayShort = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const questionArrayMid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const questionArrayLong = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+function generateQuestion (num1, num2) {
+    randomNumberS = Math.floor(Math.random() * 10);
+    randomNumberM = Math.floor(Math.random() * 12);
+    randomNumberL = Math.floor(Math.random() * 20);
+    num1 = questionArrayShort[randomNumberS];
+    num2 = questionArrayMid[randomNumberM];
+    
+    let answer = num1*num2;
+    console.log(`${num1} x ${num2} = `);
+    console.log('');//spare line to space it out
+
+    //Show square numbers
+    let square = num2*num2;
+    console.log('Here is the square number to help you, count up or down...');
+    console.log(`${num2} x ${num2} = ${square}`);
+    console.log('');//spare line to space it out
+
+    //cycle through the array to prompt the answer
+    const resultsArray = questionArrayLong.map(x => x * num2);
+    console.log(`Here is the Times Table for ${num2}`);
+    //console.log(resultsArray);
+
+    for (let count = 0; count < questionArrayLong.length; count++) {
+        console.log(questionArrayLong[count] + ' x ' + num2 + ' = ' + resultsArray[count])
+    }
+
+}
+
+//use arrays to generate a question
+generateQuestion()
