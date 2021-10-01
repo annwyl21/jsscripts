88beae0c582cebc453077745c612b86d26dee206
annwyl21 <ellen.a.ash@gmail.com>
2021-10-01 13:08:47 +0000

    now includes savings and debt

--- MumsCode/compoundInterest.js
+++ MumsCode/compoundInterest.js
@@ -1,20 +1,28 @@
 //Compound Interest Project
 
-function calculateInterest(amount, rate, term) {
-    console.log(`You save ${amount}, the fixed annual interest rate is ${rate} percent.`)
+function calculateInterest(amount, rate, term, repayment=0) {
     let account = [amount];
-    let multiplier = rate/100
-    earned = Math.floor(amount*multiplier);
-    total = Math.floor(amount + earned);
-    account.unshift(total);
-    console.log(`Year 1 £ ${earned} interest = total £ ${total} `)
-        for (let count = 1; count<term; count++) {
-        year = count+1;
-        newInterest = Math.floor(account[0] * multiplier);
-        newMoney = Math.floor(account[0] + newInterest);
-        account.unshift(newMoney);
-        console.log(`Year ${year} £ ${newInterest} interest = total £ ${newMoney} `)
-    }
+    let multiplier = rate/100;
+    if (amount <0) {
+        console.log(`Amount borrowed £ ${Math.abs(amount)}`);
+        console.log(`Annual Repayment amount £ ${repayment}`)
+    }else {
+        console.log(`Amount saved £ ${amount}`);
+        console.log(`Annual Savings Top Up Amount £ ${repayment}`)
+        }
+    console.log(`The fixed annual interest rate is ${rate} percent.`);
+        for (let count = 0; count<term; count++) {
+            year = count+1;
+            newInterest = Math.floor(account[0] * multiplier);
+            newMoney = Math.floor(account[0] + newInterest + repayment);
+            account.unshift(newMoney);
+            console.log(`Year ${year} £ ${newInterest} interest = total £ ${newMoney} `)
+    };
 }
 
-calculateInterest(-100, 28.5, 3);
+calculateInterest(100, 28.5, 3);
+console.log('');//blank line
+calculateInterest(-100, 28.5, 3, 50);
+
+//Can I save towards a goal
+//How long will it take to repay my mortgage
