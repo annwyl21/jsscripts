1e96dc19ad1e06193f119a9ae01fb9a3a610da4c
annwyl21 <ellen.a.ash@gmail.com>
2021-10-13 18:22:52 +0000

    add schools project

--- MumsCode/class.js
+++ MumsCode/class.js
@@ -153,3 +153,76 @@ class HospitalEmployee {
   speed.addRating(5);
   console.log(speed.getAverageRating());
   
+  class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get level() {
+      return this._level;
+    }
+  
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+  
+    set numberOfStudents(newValue) {
+      if (NewValue.typeOf === Number) {
+        this.numberOfStudents = newValue} else {
+          console.log("Invalid input: numberOfStudents must be set to a Number.")
+        };
+    }
+  
+    quickFacts() {
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
+    }
+  
+    static pickSubstituteTeacher(substituteTeachers) {
+      let numberOfTeachers = substituteTeachers.length-1;
+      let ranNumber = Math.floor(Math.random()*numberOfTeachers);
+      return substituteTeachers[ranNumber];
+    }
+  }
+  
+  class Primary extends School {
+    constructor(name, numberOfStudents, pickUpPolicy) {
+      super(name, 'Primary', numberOfStudents);
+      this._pickUpPolicy = pickUpPolicy;
+    }
+  
+    get pickUpPolicy() {
+      return this._pickUpPolicy;
+    }
+  }
+  
+  class Middle extends School {
+  constructor(name, numberOfStudents) {
+    super(name, 'Middle', numberOfStudents);
+  }
+  }
+  
+  class High extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'High', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+  
+    get sportsTeams() {
+      console.log(this._sportsTeams);
+    }
+  }
+  
+  const lorraineHansbury = new Primary('Lorraine Hansbury', '514', 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+  lorraineHansbury.quickFacts();
+  
+  console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+  
+  const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+  
+  alSmith.sportsTeams;
