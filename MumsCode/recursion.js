1b9d012587002d6b721eb4eb84fef1018597ef36
annwyl21 <ellen.a.ash@gmail.com>
2021-11-22 13:15:22 +0000

    fix negative numbers

--- MumsCode/recursion.js
+++ MumsCode/recursion.js
@@ -3,24 +3,20 @@ function min (x, y) {
     return Math.min(x,y);
 }
 
-console.log(min(5,8));
+//console.log(min(5,8));
 
 //recursion
 function isEven (num) {
     number = Math.abs(num);
-    function innerFunction(number){
-        result = number;
-        while (result>1){
-            result = result-2;
-        }
-        return result;
-    }
-    innerFunction(number);
-    if (result === 0){
-        return `${num} is even.`
-    }else if (result === 1){
-        return `${num} is odd.`
-    }
+    if (number >1){
+        number = number-2;
+        isEven(number);
+    };
+    if (number === 0){
+        return `${num} is even.`;
+    }else if (number === 1){
+        return `${num} is odd.`;
+    };
 }
 
 console.log(isEven(50));
@@ -29,3 +25,5 @@ console.log(isEven(-1));
 console.log(isEven(-2));
 console.log(isEven(1));
 console.log(isEven(0));
+
+console.log(isEven(2));
